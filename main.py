eb8a25be2f00996bcafe1891b2138bba52e8a1b5
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-10-18 23:49:29 +0300

    Final

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ class FlatIterator:
 
     def __init__(self, nested_list):
         self.nested_list = nested_list
-        self.flat_list = []
         self.item = -1
-        for i in self.nested_list:
-            if isinstance(i, list):
-                self.flat_list.extend(i)
-            else:
-                self.flat_list.append(i)  # Заготовка под задачу со *
+
+        while True:  # Крутит цикл пока не уберет все вложенные list
+            stop = True
+            self.flat_list = []
+            for i in self.nested_list:
+                if isinstance(i, list):
+                    self.flat_list.extend(i)
+                else:
+                    self.flat_list.append(i)
+            for i in self.flat_list:
+                if isinstance(i, list):
+                    stop = False
+                    self.nested_list = self.flat_list
+                    break
+            if stop is True:
+                break
 
     def __iter__(self):
         return self
@@ -24,9 +34,10 @@ def iterator_test():
     """Iterator test function"""
 
     nested_list = [
-        ['a', 'b', 'c'],
+        ['a', ['a', 'b', 'c'], 'c'],
         ['d', 'e', 'f', 'h', False],
-        [1, 2, None]
+        [1, 2, None],
+        2
     ]
 
     for item in FlatIterator(nested_list):
@@ -40,12 +51,21 @@ def flat_generator(nested_list):
     """generator"""
 
     item = 0
-    flat_list = []
-    for i in nested_list:
-        if isinstance(i, list):
-            flat_list.extend(i)
-        else:
-            flat_list.append(i)  # Заготовка под задачу со *
+    while True:  # Крутит цикл пока не уберет все вложенные list
+        stop = True
+        flat_list = []
+        for i in nested_list:
+            if isinstance(i, list):
+                flat_list.extend(i)
+            else:
+                flat_list.append(i)
+        for i in flat_list:
+            if isinstance(i, list):
+                nested_list = flat_list
+                stop = False
+        if stop is True:
+            break
+
     while item < len(flat_list):
         yield flat_list[item]
         item += 1
@@ -55,9 +75,10 @@ def generator_test():
     """Generator test function"""
 
     nested_list = [
-        ['a', 'b', 'c'],
+        ['a', ['d', 'e', ['d', 'e', 'f']], 'c'],
         ['d', 'e', 'f'],
         [1, 2, None],
+        2
     ]
     for item in flat_generator(nested_list):
         print(item)
@@ -66,8 +87,8 @@ def generator_test():
 def main():
     """Main function"""
 
-    # iterator_test() # Задача 1
-    # generator_test() # Задача 2
+    # iterator_test()  # Задача 1
+    # generator_test()  # Задача 2
 
 
 if __name__ == '__main__':
